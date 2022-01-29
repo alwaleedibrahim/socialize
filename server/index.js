99c09a6c6ae16d54e975598f2068f082f799262b
alwaleedibrahim <alwaleedibramohammed@gmail.com>
2022-01-29 11:42:03 +0200

    Added forbidden message on server

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const express = require("express")
 const app = express()
 
 app.get("/", (req, res)=> {
-    res.send("BackEnd works")
+    res.status(403).send("<h1>Error 403 - Forbidden</h1><p>You don't have permission to access / on this server</p><hr>")
 })
 app.listen(3000, () => {
     console.log("App is running on port 3000")
