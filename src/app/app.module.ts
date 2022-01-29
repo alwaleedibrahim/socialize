408add019f1af10eb8f010a61b60d0259eb588a1
alwaleedibrahim <alwaleedibramohammed@gmail.com>
2022-01-29 09:40:41 +0200

    Added folder structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { FooterComponent } from './shared/footer/footer.component';
-import { LoginComponent } from './pages/login/login.component';
+import { LoginComponent } from './pages/user/login//login.component';
+import { SignupComponent } from './pages/user/signup/signup.component';
+import { ProfileComponent } from './pages/user/profile/profile.component';
+import { UpdateComponent } from './pages/user/update/update.component';
+import { IndexComponent } from './pages/posts/index/index.component';
+import { ShowComponent } from './pages/posts/show/show.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    LoginComponent
+    LoginComponent,
+    SignupComponent,
+    ProfileComponent,
+    UpdateComponent,
+    IndexComponent,
+    ShowComponent
   ],
   imports: [
     BrowserModule,
